f7da8be38a1149837abce4ee07a4b59e3f1ca655
Xin Li <lixin@shopeemobile.com>
2018-10-11 17:43:49 +0800

    lab3

---
+++ lab3/Flik/testFilk.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class testFilk {
+
+    @Test
+    public void testHorribleSteve() {
+        assertEquals(true, Flik.isSameNumber(128, 128));
+    }
+
+
+}
